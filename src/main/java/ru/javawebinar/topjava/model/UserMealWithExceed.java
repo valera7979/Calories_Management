64ff32f377681c796afedd3737ea3b11c5299eec
valera7979 <valera7979@mail.ru>
2017-03-21 18:37:41 +0200

    HW0 with streams

--- src/main/java/ru/javawebinar/topjava/model/UserMealWithExceed.java
+++ src/main/java/ru/javawebinar/topjava/model/UserMealWithExceed.java
@@ -33,4 +33,14 @@ public class UserMealWithExceed {
     public boolean isExceed() {
         return exceed;
     }
+
+    @Override
+    public String toString() {
+        return "UserMealWithExceed{" +
+                "dateTime=" + dateTime +
+                ", description='" + description + '\'' +
+                ", calories=" + calories +
+                ", exceed=" + exceed +
+                '}';
+    }
 }
