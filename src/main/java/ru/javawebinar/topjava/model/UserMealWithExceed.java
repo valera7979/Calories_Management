95023aa5eb09920111d99127cf594a5547b1088e
valera7979 <valera7979@mail.ru>
2017-04-01 13:51:39 +0300

    2 3 app layers

--- src/main/java/ru/javawebinar/topjava/model/UserMealWithExceed.java
+++ src/main/java/ru/javawebinar/topjava/model/UserMealWithExceed.java
@@ -2,10 +2,7 @@ package ru.javawebinar.topjava.model;
 
 import java.time.LocalDateTime;
 
-/**
- * GKislin
- * 11.01.2015.
- */
+
 public class UserMealWithExceed {
     private final LocalDateTime dateTime;
 
