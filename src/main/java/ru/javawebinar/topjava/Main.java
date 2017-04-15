e5b4a3b7b62dcc604b04b0edc35f3f01c047bfba
valera7979 <valera7979@mail.ru>
2017-04-15 12:17:57 +0300

    4 0 fix

--- src/main/java/ru/javawebinar/topjava/Main.java
+++ src/main/java/ru/javawebinar/topjava/Main.java
@@ -1,8 +1,13 @@
 package ru.javawebinar.topjava;
 
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.format("Hello Topjava Enterprise!");
+
     }
 }
